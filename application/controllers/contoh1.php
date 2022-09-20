cc87030ed4c66aa03324f1805f2889e9e5819744
Shabilla Rahmadieta <19210471@bsi.ac.id>
2022-09-20 16:47:34 +0700

    menambahkan puasaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Shabilla Rahmadieta - 19210471<br>
+            saya tinggal di Bekasi<br>
+            saya kuliah di universitas<br> 
+            Bina Sarana Informatika";
+    }
+}
